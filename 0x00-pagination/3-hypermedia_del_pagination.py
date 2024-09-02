edab79469e9d5c87b1b52740e11d61bd907c3977
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-02 23:39:01 +0300

    Task 3 : Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ Get hypermedia pagination """
+
+        dataset = self.indexed_dataset()
+        index = index if index else 0
+        dic_keys = sorted(dataset.keys())
+        assert index >= 0 and index <= dic_keys[-1]
+        index_list = []
+        for i in dic_keys:
+            if i >= index and len(index_list) <= page_size:
+                index_list.append(i)
+        data = []
+        for j in index_list[:-1]:
+            data.append(dataset[j])
+        if len(index_list) - page_size == 1:
+            next_index = index_list[-1]
+        else:
+            next_index = None
+        return {
+            "index": index_list[0],
+            "data": data,
+            "page_size": len(data),
+            "next_index": next_index
+            }
