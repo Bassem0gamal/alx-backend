00c3177897fbbcf54843c66ba099ad7b844162e4
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-05 02:37:47 +0300

    Task 2 : LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" LIFO caching"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO cache class """
+
+    def __init__(self):
+        """ Override superclass __init__ """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data:
+                discard = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(discard)
+                print("DISCARD: {}".format(discard))
+
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item from the cache """
+        if key:
+            return self.cache_data.get(key)
