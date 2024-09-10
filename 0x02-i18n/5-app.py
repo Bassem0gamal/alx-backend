fa4a465bdbfdc3dd45e466f7b30c4092209b2cf2
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-10 19:35:20 +0300

    Task 5 : Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+""" Back to flask """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ Flask configuration class """
+    LANGUAGES: list = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app: Flask = Flask(__name__)
+app.config.from_object(Config())
+babel: Babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get best localized language """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user() -> dict:
+    """ Get user from login_as parameter """
+    user_id = request.args.get('login_as')
+    if user_id is None:
+        return None
+    return users.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    """ Set user before request """
+    g.user = get_user()
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """ Index route """
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
